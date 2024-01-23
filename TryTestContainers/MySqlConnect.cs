d1904ff8dc5cc77a6acc2b9ed96c248c802df8f5
agile.zhou <kklldog@live.cn>
2024-01-24 01:23:28 +0800

    添加项目文件。

---
+++ TryTestContainers/MySqlConnect.cs
@@ -0,0 +1,17 @@
+﻿using MySqlConnector;
+
+namespace TryTestContainers
+{
+    public class MySqlConnect
+    {
+        public bool TryConnect(string connectionString)
+        {
+            using (var connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                return true;
+            }
+        }
+    }
+}
---
+++ TryTestContainersTests/MySqlConnectTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TryTestContainers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Testcontainers.MySql;
+
+namespace TryTestContainers.Tests
+{
+    [TestClass()]
+    public class MySqlConnectTests
+    {
+        MySqlContainer _container = new MySqlBuilder().WithImage("mysql:8.0").Build();
+
+        [TestInitialize]
+        public async Task Init()
+        {
+            await _container.StartAsync();
+        }
+
+        [TestCleanup]
+        public async Task Clean()
+        {
+            await _container.DisposeAsync();
+        }
+
+        [TestMethod()]
+        public void TestTryConnect_Ok()
+        {
+            // arrange
+            var connector = new MySqlConnect();
+
+            // act
+            var connected = connector.TryConnect(_container.GetConnectionString());
+
+            // assert
+            Assert.IsTrue(connected);
+        }
+    }
+}
